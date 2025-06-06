e8cac9489083f1c8cd62d78641f9f52f3d57e8c2
DESKTOP-32DIJOJ\User <kulish.y@ajax.systems>
2025-06-06 23:01:24 +0300

    Second programm

--- hw_01.py
+++ hw_01.py
@@ -1,23 +1,19 @@
 from datetime import datetime
 
+target_day = input("Задайте необхідну дату у форматі РРРР-ММ-ДД: ")
+current_date = datetime.now().today().date()
+print(f'Поточна дата: {current_date}')
+
 try:
-    target_day = input("Задайте необхідну дату у форматі РРРР-ММ-ДД: ")
-    current_date = datetime.now().today().date()
-    print(f'Поточна дата: {current_date}')
-
-
-    # Функція для перетворення рядка дати у форматі "РРРР-ММ-ДД" у об'єкт datetime
-    def string_to_date(date_string):
-        return datetime.strptime(date_string, "%Y-%m-%d").date()
-
-
-    # Функція, що розраховує кількість днів між заданою датою та поточною
-    def get_days_from_today(date):
-        difference_date = string_to_date(target_day) - current_date
-        return difference_date.days
-
-
-    print(f'Різниця між заданою датою та поточною: {get_days_from_today(target_day)} днів')
-
 except ValueError:
     print(f'Невірний формат необхідної дати.')
+
+ # Функція для перетворення рядка дати у форматі "РРРР-ММ-ДД" у об'єкт datetime
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y-%m-%d").date()
+
+# Функція, що розраховує кількість днів між заданою датою та поточною
+def get_days_from_today(date):
+    difference_date = string_to_date(target_day) - current_date
+    return difference_date.days
+print(f'Різниця між заданою датою та поточною: {get_days_from_today(target_day)} днів')
--- hw_02.py
+++ hw_02.py
@@ -1,22 +1,20 @@
 from random import sample
 
+# Функція, що генерує вказану кількість унікальних чисел у заданому діапазоні
+def get_numbers_ticket(min , max, quantity):
+    if min >= 1 and max <= 1000:
+        return sorted(sample(range(min, max), quantity))
+    else:
+        return list()
+
 try:
     min = int(input('Введіть мінімальне значення лотерейного квитка: '))
     max = int(input('Введіть максимальне значення лотерейного квитка: '))
     quantity = int(input("Введіть кількість вгаданих чисел у квитку: "))
 
+    lottery_numbers = get_numbers_ticket(min, max, quantity)
 
-    # Функція, що генерує вказану кількість унікальних чисел у заданому діапазоні
-    def get_numbers_ticket(min, max, quantity):
-        if min >= 1 and max <= 1000:
-            lottery_numbers = sorted(sample(range(min, max), quantity))
-            return lottery_numbers
-
-        else:
-            return list()
-
-
-    print(f'Ваші лотерейні числа {get_numbers_ticket(min, max, quantity)}')
+    print(f'Ваші лотерейні числа {lottery_numbers}')
 
 except ValueError:
-    print(f'Невірний формат лотерейних чисел.')
+    print(f'Невірний формат лотерейного квитка ')
